03b12c4719a06733192efcb034d7e985899474a1
AMathur2005 <anubhavmathurch@gmail.com>
2025-01-04 14:52:15 +0000

    forcing error

--- UnitTests.py
+++ UnitTests.py
@@ -9,7 +9,7 @@ class TestFunction(unittest.TestCase):
         file_path = "Births outside of marriage.csv"
         df = pd.read_csv(file_path)
         row_count = len(df)
-        expected_row_count = 1876
+        expected_row_count = 1877
     
 
 if __name__ == '__main__':
