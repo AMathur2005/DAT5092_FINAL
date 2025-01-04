f1c9d296fa10bf19484d0a77bd737db205528843
AMathur2005 <anubhavmathurch@gmail.com>
2025-01-04 14:58:38 +0000

    correcting unittest

--- UnitTests.py
+++ UnitTests.py
@@ -3,13 +3,21 @@ import pandas as pd
 # check if number of rows is 1876
 # if not, fail, if yes, true
 
+def row_count():
+    file_path = "Births outside of marriage.csv"
+    df = pd.read_csv(file_path)
+    row_count = len(df)
+    expected_row_count = 1876
+    if row_count != expected_row_count:
+        return False
+    else:
+        return True
+
 
 class TestFunction(unittest.TestCase):
     def test_row_count(self):
-        file_path = "Births outside of marriage.csv"
-        df = pd.read_csv(file_path)
-        row_count = len(df)
-        expected_row_count = 1877
+        result = row_count()
+        self.assertEqual(result,True)
     
 
 if __name__ == '__main__':
