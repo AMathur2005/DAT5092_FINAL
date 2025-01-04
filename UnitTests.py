4ff67bceae26f35af50abb07d0b4f3f726ffc5f6
AMathur2005 <anubhavmathurch@gmail.com>
2025-01-04 14:47:26 +0000

    First unittest

--- UnitTests.py
+++ UnitTests.py
@@ -1,5 +1,16 @@
 import unittest
+import pandas as pd
+# check if number of rows is 1876
+# if not, fail, if yes, true
 
+
+class TestFunction(unittest.TestCase):
+    def test_row_count(self):
+        file_path = "Births outside of marriage.csv"
+        df = pd.read_csv(file_path)
+        row_count = len(df)
+        expected_row_count = 1876
+    
 
 if __name__ == '__main__':
     unittest.main()
